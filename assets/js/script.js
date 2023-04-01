6190842b71319c7d1cc4345dea350b170fb49f53
Ashleyg5 <ashleygiraldo5@hotmail.com>
2023-04-01 19:44:08 -0400

    added extra div to html for questions/answers, made some changes in JS and CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,27 @@ var count = 75;
 var currentQuestion = 0;
 var score = 0;
 var totalQuestions = questions.length;
-var container = document.getElementById("#quizContainer");
-var questionEl = document.getElementById("question");
-var opt1 = document.getElementById("#opt1");
-var opt2 = document.getElementById("#opt2");
-var opt3 = document.getElementById("#opt3");
-var opt4 = document.getElementById("#opt4");
-var resultCont = document.getElementById("#result");
+var startContainer = document.querySelector("#starterContainer");
+var quizContainer = document.querySelector("#quizContainer");
 
+startBtn.addEventListener("click", function(event) {
+    var element = event.target;
+    var id = element.getAttribute("data-state");
 
-startBtn.addEventListener("click", function() {
-    startQuiz();
+    if (element.getAttribute("data-state") === "shown") {
+        element.innerHTML = id;
+        element.setAttribute("data-state", "hidden");   
+    }
+    
+    if (element.getAttribute("data-state") === "hidden") {
+        element.setAttribute("data-state", "shown");
+    }
+
+    startQuiz()
 });
 
 function startQuiz() {
+    
     var timerInterval = setInterval(function() {
         count--;
         timerEl.textContent = "Time: " + count;
@@ -27,15 +34,7 @@ function startQuiz() {
         }
 
     }, 1000);
+
     
-    loadQuestion();
 }
 
-function loadQuestion(questionIndex) {
-    var q = questions[questionIndex];
-    questionEl.textContent = (questionIndex + 1) + "." + q.questions;
-    opt1.textContent = q.option1;
-    opt2.textContent = q.option2;
-    opt3.textContent = q.option3;
-    opt4.textContent = q.option4;
-}
