445fc113cdd6c27291a59b524997f8687a1f86a7
Ashleyg5 <ashleygiraldo5@hotmail.com>
2023-04-01 16:20:41 -0400

    created question.js to store all questions, edited items in css and js, added question div in index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,20 @@
 var timerEl = document.querySelector("#timer");
 var startBtn = document.querySelector("#start");
 var count = 75;
+var currentQuestion = 0;
+var score = 0;
+var totalQuestions = questions.length;
+var container = document.getElementById("#quizContainer");
+var questionEl = document.getElementById("#question");
+var opt1 = document.getElementById("#opt1");
+var opt2 = document.getElementById("#opt2");
+var opt3 = document.getElementById("#opt3");
+var opt4 = document.getElementById("#opt4");
+var resultCont = document.getElementById("#result");
+
 
 startBtn.addEventListener("click", function() {
- startQuiz();
+    startQuiz();
 });
 
 function startQuiz() {
@@ -16,5 +27,7 @@ function startQuiz() {
         }
 
     }, 1000);
+    
+
 }
 
