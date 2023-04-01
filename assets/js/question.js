52467086ff619b57a376889b70c19eee2070b85a
Ashleyg5 <ashleygiraldo5@hotmail.com>
2023-04-01 16:46:29 -0400

    added questions to the question.js file

--- assets/js/question.js
+++ assets/js/question.js
@@ -1,8 +1,38 @@
 var questions = [{
+    "question":"The first mechanical computer designed by Charles Babbage was called ?",
+    "option1":"Abacus",
+    "option2":"Analytical Engine",
+    "option3":"Calculator",
+    "option4":"Processor",
+    "answer":"2"
+}, {
+    "question":"In which HTML elements do we put in JavaScript code?",
+    "option1":"<js>",
+    "option2":"<script>",
+    "option3":"<body>",
+    "option4":"<head>",
+    "answer":"2"
+}, {
+    "question":"Which HTML attribute is used to reference an external JavaScript file?",
+    "option1":"<src>",
+    "option2":"<p>",
+    "option3":"<footer>",
+    "option4":"<Java>",
+    "answer":"1"
+}, {
+    "question":"What does HTML stand for?",
+    "option1":"Hyper Text Preprocessor",
+    "option2":"Hyper Text Multiple Language",
+    "option3":"Hyper Tool Multi Language",
+    "option4":"Hyper Text Markup Langauge",
+    "answer":"4"
+}, {
+    "question":"What does JavaScript provide to code?",
+    "option1":"Curb Appeal",
+    "option2":"The Foundation",
+    "option3":"Functionality",
+    "option4":"None Of The Above",
+    "answer":"3"
+}
 
-
-
-
-
-    
-}]
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var currentQuestion = 0;
 var score = 0;
 var totalQuestions = questions.length;
 var container = document.getElementById("#quizContainer");
-var questionEl = document.getElementById("#question");
+var questionEl = document.getElementById("question");
 var opt1 = document.getElementById("#opt1");
 var opt2 = document.getElementById("#opt2");
 var opt3 = document.getElementById("#opt3");
@@ -28,6 +28,14 @@ function startQuiz() {
 
     }, 1000);
     
-
+    loadQuestion();
 }
 
+function loadQuestion(questionIndex) {
+    var q = questions[questionIndex];
+    questionEl.textContent = (questionIndex + 1) + "." + q.questions;
+    opt1.textContent = q.option1;
+    opt2.textContent = q.option2;
+    opt3.textContent = q.option3;
+    opt4.textContent = q.option4;
+}
