d7d4a39adf332c0758a2f2aa7eabd5b509e8923a
Ashleyg5 <ashleygiraldo5@hotmail.com>
2023-04-02 14:37:31 -0400

    made changesi n JS added questions/answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,64 @@
 var timerEl = document.querySelector("#timer");
 var startBtn = document.querySelector("#start");
 var count = 75;
-var currentQuestion = 0;
-var score = 0;
-var totalQuestions = questions.length;
+var totalQuestions = question.length;
 var startContainer = document.querySelector("#starterContainer");
 var quizContainer = document.querySelector("#quizContainer");
+var question = document.querySelector("question");
+var qIndex = 0;
 
-startBtn.addEventListener("click", function(event) {
-    var element = event.target;
-    var id = element.getAttribute("data-state");
-
-    if (element.getAttribute("data-state") === "shown") {
-        element.innerHTML = id;
-        element.setAttribute("data-state", "hidden");   
+var myQuestions = [
+    {
+        q: "In which HTML elements do we put in JavaScript code?",
+        a: {
+            1: "<js>",
+            2: "<script>",
+            3: "<body>",
+            4: "<head>"
+        },
+        correctAns: "2"
+    },
+    {
+        q: "Which HTML attribute is used to reference an external JavaScript file?",
+        a: {
+            1: "<src>",
+            2: "<p>",
+            3: "<footer>",
+            4: "<Java>"
+        },
+        correctAns: "1"
+    },
+    {
+        q: "What does HTML stand for?",
+        a: {
+            1: "Hyper Text Preprocessor",
+            2: "Hyper Text Multiple Language",
+            3: "Hyper Tool Multi Language",
+            4: "Hyper Text Markup Langauge"
+        },
+        correctAns: "4"
+    },
+    {
+        q: "What does JavaScript provide to code?",
+        a: {
+            1: "Curb Appeal",
+            2: "The Foundation",
+            3: "Functionality",
+            4: "None Of The Above"
+        },
+        correctAns: "3"
     }
-    
-    if (element.getAttribute("data-state") === "hidden") {
-        element.setAttribute("data-state", "shown");
-    }
+];
 
-    startQuiz()
+
+startBtn.addEventListener("click", function() {
+    startTime();
+    startContainer.classList.add("hidden");
+    quizContainer.classList.remove("hidden");
+    showQuestion();
 });
 
-function startQuiz() {
+function startTime() {
     
     var timerInterval = setInterval(function() {
         count--;
@@ -34,7 +69,16 @@ function startQuiz() {
         }
 
     }, 1000);
-
     
+};
+
+function showQuestion() {
+
+
+
 }
 
+  // <button class="option">Option 1</button>
+  // <button class="option">Option 2</button>
+  // <button class="option">Option 3</button>
+  // <button class="option">Option 4</button>
