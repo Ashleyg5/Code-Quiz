b9920f856fef5568660bba6dc412167691207407
Ashleyg5 <ashleygiraldo5@hotmail.com>
2023-04-03 17:03:31 -0400

    created functions ins JS for questions. Worked with a tutor to get the functions working. Created a div in HTML for end score

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,20 @@ var startBtn = document.querySelector("#start");
 var count = 75;
 var startContainer = document.querySelector("#starterContainer");
 var quizContainer = document.querySelector("#quizContainer");
-var question = document.querySelector("question");
+var question = document.querySelector("#question");
 var result = document.querySelector("#result");
+var opt1 = document.querySelector("#opt1");
+var opt2 = document.querySelector("#opt2");
+var opt3 = document.querySelector("#opt3");
+var opt4 = document.querySelector("#opt4");
+var answerBtn = document.querySelector("#answerBtns");
+var initials = document.querySelector("#initials");
+var timerInterval;
+var score = document.querySelector("#score");
+var input = document.querySelector("#initialInput");
+var initialBtn = document.querySelector("#initialBtn");
 var qIndex = 0;
-//  innerHTML = ""
+
 
 var myQuestions = [
     {
@@ -17,7 +27,7 @@ var myQuestions = [
             3: "<body>",
             4: "<head>"
         },
-        correctAns: "2"
+        correctAns: "<script>"
     },
     {
         q: "Which HTML attribute is used to reference an external JavaScript file?",
@@ -27,7 +37,7 @@ var myQuestions = [
             3: "<footer>",
             4: "<Java>"
         },
-        correctAns: "1"
+        correctAns: "<src>"
     },
     {
         q: "What does HTML stand for?",
@@ -37,7 +47,7 @@ var myQuestions = [
             3: "Hyper Tool Multi Language",
             4: "Hyper Text Markup Langauge"
         },
-        correctAns: "4"
+        correctAns: "Hyper Text Markup Langauge"
     },
     {
         q: "What does JavaScript provide to code?",
@@ -47,9 +57,19 @@ var myQuestions = [
             3: "Functionality",
             4: "None Of The Above"
         },
-        correctAns: "3"
+        correctAns: "Functionality"
     }
 ];
+    function newQuestion() {
+
+    question.textContent = myQuestions[qIndex].q;
+        opt1.textContent = myQuestions[qIndex].a[1];
+        opt2.textContent = myQuestions[qIndex].a[2];
+        opt3.textContent = myQuestions[qIndex].a[3];
+        opt4.textContent = myQuestions[qIndex].a[4];
+
+
+    }
 
 function startQuiz() {
 
@@ -60,7 +80,7 @@ function startQuiz() {
     
     function startTime() {
 
-        var timerInterval = setInterval(function () {
+         timerInterval = setInterval(function () {
             count--;
             timerEl.textContent = "Time: " + count;
 
@@ -70,16 +90,8 @@ function startQuiz() {
 
         }, 1000);
     }
-    function newQuestion() {
-
-        question.innerHTML = myQuestions[qIndex].question
-
-
-    }
 
 //  result.classList.remove("hidden");
-
-
 
     startTime();
     newQuestion();
@@ -87,5 +99,34 @@ function startQuiz() {
 
 
 //sound effects on correct/ incorrect
+initialBtn.addEventListener("click", function() {
+    var scores = JSON.parse(localStorage.getItem("scores")) || [];
+    scores.push({name:input.value,score:count});
+    localStorage.setItem("scores", JSON.stringify(scores));
+})
 
 startBtn.addEventListener("click", startQuiz);
+
+answerBtn.addEventListener("click", function (event) {
+
+    if (event.target.nodeName !== "BUTTON") {
+        return;
+    } 
+    var userChoice = event.target.innerText
+    var correctAns = myQuestions[qIndex].correctAns
+
+    if (userChoice !== correctAns) {
+       count-= 10;//shorthand for count= count - 10
+    } 
+    qIndex++
+    if (qIndex >= myQuestions.length) {
+    quizContainer.classList.add("hidden");
+        initials.classList.remove("hidden");
+        clearInterval(timerInterval);
+        document.querySelector("#score").innerText = "YOUR SCORE " + count;
+        return;
+
+    }
+    newQuestion();
+})
+
