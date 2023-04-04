5785bc5354274ee7cf6e34839a9d63b6b05607da
Ashleyg5 <ashleygiraldo5@hotmail.com>
2023-04-04 18:34:13 -0400

    edited JS, added eventlisteners for highscore buttons, saved and displayed localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var timerEl = document.querySelector("#timer");
 var startBtn = document.querySelector("#start");
-var count = 75;
+var scoreBtn = document.querySelector("#scorelink");
+var count = 50;
 var startContainer = document.querySelector("#starterContainer");
 var quizContainer = document.querySelector("#quizContainer");
 var question = document.querySelector("#question");
@@ -15,7 +16,11 @@ var timerInterval;
 var score = document.querySelector("#score");
 var input = document.querySelector("#initialInput");
 var initialBtn = document.querySelector("#initialBtn");
+var backBtn = document.querySelector("#goback");
 var qIndex = 0;
+var highscore = document.querySelector("#highscore");
+var playC = new Audio("./assets/audio/correct.mp3");
+var playI = new Audio("./assets/audio/incorrect.wav");
 
 
 var myQuestions = [
@@ -58,75 +63,164 @@ var myQuestions = [
             4: "None Of The Above"
         },
         correctAns: "Functionality"
+    },
+    {
+        q: "What does CSS stand for?",
+        a: {
+            1: "Cascading Style Sheets",
+            2: "Colored Style Sheets",
+            3: "Coding Style Sheets",
+            4: "Coding Submission Sheets"
+        },
+        correctAns: "Cascading Style Sheets"
     }
 ];
-    function newQuestion() {
+
+function newQuestion() {
 
     question.textContent = myQuestions[qIndex].q;
-        opt1.textContent = myQuestions[qIndex].a[1];
-        opt2.textContent = myQuestions[qIndex].a[2];
-        opt3.textContent = myQuestions[qIndex].a[3];
-        opt4.textContent = myQuestions[qIndex].a[4];
+    opt1.textContent = myQuestions[qIndex].a[1];
+    opt2.textContent = myQuestions[qIndex].a[2];
+    opt3.textContent = myQuestions[qIndex].a[3];
+    opt4.textContent = myQuestions[qIndex].a[4];
 
+    return;
 
-    }
+}
 
 function startQuiz() {
 
     startContainer.classList.add("hidden");
     quizContainer.classList.remove("hidden");
-   
 
-    
+
+
     function startTime() {
 
-         timerInterval = setInterval(function () {
+        timerInterval = setInterval(function () {
             count--;
             timerEl.textContent = "Time: " + count;
 
-            if (count === 0) {
+            if (count <= 0) {
                 clearInterval(timerInterval);
+                alert("Times Up!");
+
             }
 
         }, 1000);
     }
 
-//  result.classList.remove("hidden");
-
     startTime();
     newQuestion();
 }
 
+backBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    initials.classList.add("hidden");
+    startContainer.classList.remove("hidden");
+    clearInterval(timerInterval);
+    count = 50;
+    timerEl.textContent = "Time: " + 0;
+});
 
-//sound effects on correct/ incorrect
-initialBtn.addEventListener("click", function() {
+
+initialBtn.addEventListener("click", function () {
     var scores = JSON.parse(localStorage.getItem("scores")) || [];
-    scores.push({name:input.value,score:count});
+    scores.push({ name: input.value, score: count });
     localStorage.setItem("scores", JSON.stringify(scores));
+    endGame();
 })
+
+function endGame () {
+
+}
 
 startBtn.addEventListener("click", startQuiz);
 
 answerBtn.addEventListener("click", function (event) {
 
+
     if (event.target.nodeName !== "BUTTON") {
         return;
-    } 
+    }
     var userChoice = event.target.innerText
     var correctAns = myQuestions[qIndex].correctAns
 
     if (userChoice !== correctAns) {
-       count-= 10;//shorthand for count= count - 10
-    } 
+        count -= 10;//shorthand for count= count - 10
+        timerEl.textContent = "Time: " + count;
+        playI.play()
+
+        if (count <= 0) {
+            clearInterval(timerInterval);
+            alert("Times Up!");
+            score = 0;
+        }
+    } else { 
+    playC.play() }
     qIndex++
     if (qIndex >= myQuestions.length) {
-    quizContainer.classList.add("hidden");
+        quizContainer.classList.add("hidden");
         initials.classList.remove("hidden");
+        timerEl.textContent = "Time: " + count;
         clearInterval(timerInterval);
-        document.querySelector("#score").innerText = "YOUR SCORE " + count;
+        document.querySelector("#score").innerText = "YOUR SCORE: " + count;
+        
+        if (count <= 0) {
+            clearInterval(timerInterval);
+            alert("Times Up!");
+            score = 0;
+        }
         return;
-
     }
+
     newQuestion();
+    
 })
+
+scoreBtn.addEventListener("click", function() {
+    startContainer.classList.add("hidden");
+    highscore.classList.remove("hidden");
+
+    const string = localStorage.getItem("scores");
+    const array = JSON.parse(string);
+    console.log(array);
+    const ul = document.createElement('ul');
+    for(let i = 0; i < array.length; i++){
+      const li = document.createElement('li');
+      const text = document.createTextNode(`${array[i].name} : ${array[i].score}`);
+      li.appendChild(text);
+      ul.appendChild(li);
+    }
+   
+    document.getElementById("allscores").appendChild(ul);
+    
+})
+
+initialBtn.addEventListener("click", function () {
+    initials.classList.add("hidden");
+    highscore.classList.remove("hidden");
+
+    const string = localStorage.getItem("scores");
+    const array = JSON.parse(string);
+    console.log(array);
+    const ul = document.createElement('ul');
+    for(let i = 0; i < array.length; i++){
+      const li = document.createElement('li');
+      const text = document.createTextNode(`${array[i].name} : ${array[i].score}`);
+      li.appendChild(text);
+      ul.appendChild(li);
+    }
+   
+    document.getElementById("allscores").appendChild(ul); 
+})
+
+//NEED -
+//when timer hits 0, it goes to highscore page
+//when all answers are incorrect, the score needs to be 0 instead of going into the negative numbers
+//timer reflect final number
+//link highscore html to start as soon as the game is finished
+//link localstorage to highscore html
+//set go back button to return to index html
+//set view highscores submit button to pull up the page with all highscores after initials are inputted
 
